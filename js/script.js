f50d146dd8aaef0b73a26fcaa0c58c9b23fa5bf2
Pritom07 <pritommohajon4545@gmail.com>
2024-08-28 12:26:13 +0600

    all calculation done

--- js/script.js
+++ js/script.js
@@ -17,27 +17,26 @@ document.addEventListener("DOMContentLoaded", function () {
             li.classList.add("flex");
             li.classList.add("justify-between");
             li.classList.add("items-center");
-            ulID.appendChild(li);
-            const prevVal = getTextById("total-selected-seats");
-            const newVal = prevVal + 1;
-            setText("total-selected-seats", newVal);
-            const prevLeftSeats = getTextById("left-seat");
-            const newLeftSeats = prevLeftSeats - 1;
-            setText("left-seat", newLeftSeats);
             const currentSelectedSeats = getTextById("total-selected-seats");
-            const ticketPrice = currentSelectedSeats * 550;
-            setText("total-price", ticketPrice);
-            setText("grand-total", ticketPrice);
-            if (currentSelectedSeats == 4) {
+            if (currentSelectedSeats == 3) {
                 const applyBtn = document.getElementById("apply-btn");
                 applyBtn.removeAttribute("disabled");
                 applyBtn.classList.add("bg-[#1DD100]");
                 applyBtn.classList.add("hover:bg-[#1DD100]");
             }
-            if (currentSelectedSeats > 4) {
+            if (currentSelectedSeats > 3) {
                 alert("Sorry sir. You can't purchase more than 4 tickets at a time. Best of luck for your happy journey.");
                 return;
             }
+            ulID.appendChild(li);
+            const newVal = currentSelectedSeats + 1;
+            setText("total-selected-seats", newVal);
+            const prevLeftSeats = getTextById("left-seat");
+            const newLeftSeats = prevLeftSeats - 1;
+            setText("left-seat", newLeftSeats);
+            const ticketPrice = newVal * 550;
+            setText("total-price", ticketPrice);
+            setText("grand-total", ticketPrice);
             event.target.setAttribute("disabled", "true");
             event.target.style.backgroundColor = "LimeGreen";
             event.target.style.color = "black";
@@ -48,7 +47,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.getElementById("apply-btn").addEventListener("click", function () {
     const couponField = getInputFieldValue("coupon-field");
-
     const singleDiscount = document.getElementById("single-discount-section");
     const coupleDiscount = document.getElementById("couple-discount-section");
     if (couponField === "NEW15") {
@@ -66,3 +64,23 @@ document.getElementById("apply-btn").addEventListener("click", function () {
         return;
     }
 })
+
+document.getElementById("passengerName").addEventListener("keyup", function (event1) {
+    const passengerName = event1.target.value;
+})
+
+document.getElementById("phone-number").addEventListener("keyup", function (event2) {
+    document.getElementById("email").addEventListener("keyup", function (event3) {
+        const phoneNumber = event2.target.value;
+        const email = event3.target.value;
+        if ((email.endsWith("gmail.com")) && (!isNaN(phoneNumber))) {
+            const nextButton = document.getElementById("next-btn");
+            nextButton.removeAttribute("disabled");
+        }
+    })
+})
+
+
+
+
+
